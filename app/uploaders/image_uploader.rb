316d1db2048b2ee0db6aec91f0b8bb1f6af224ef
Parker-Wiley <pobsterlot@gmail.com>
2020-02-26 20:37:08 -0600

    amazon s3 and heroku connection via fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+if Rails.env.production?
+storage :fog
+else 
+storage :file
+end
 
 end
